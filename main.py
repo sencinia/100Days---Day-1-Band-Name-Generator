332463e2310730e8956909b5e4f6eacf3d5b2ed8
sencinia <91294669+sencinia@users.noreply.github.com>
2022-03-10 15:51:01 -0800

    Create main.py
    Completed Day 1 Challenge

---
+++ main.py
@@ -0,0 +1,8 @@
+#1. Create a greeting for your program.
+print("Welcome to Sebby's Band Name Generator!")
+#2. Ask the user for the city that they grew up in.
+city = input("Which city did you grow up in?\n")
+#3. Ask the user for the name of a pet.
+pet = input("What is your pet's name?\n")
+#4. Combine the name of their city and pet and show them their band name.
+print("And your new band name is.. " + city + " " + pet + "!")
